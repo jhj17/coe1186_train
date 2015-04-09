eff7f3cb7236b80f14c55b6fce0c8c177fd93b87
jhj17 <jeff_josephs15@yahoo.com>
2015-04-09 02:11:05 -0400

    Update MainWrapper.java

--- TrackController/MainWrapper.java
+++ TrackController/MainWrapper.java
@@ -4,18 +4,24 @@ import java.awt.EventQueue;
 import java.util.LinkedList;
 
 public class MainWrapper {
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					//SimClock clock = new SimClock(100,0,0,0);
+					//TrackModelWrapper trackModelWrapper = new TrackModelWrapper();
+					//TrackControllerWrapper trackControllerWrapper = new TrackControllerWrapper(trackModel);
+					TrackControllerWrapper trackControllerWrapper = new TrackControllerWrapper();
+					//CTCWrapper ctcWrapper = new CTCWrapper(trackModelWrapper,trackControllerWrapper,clock);
+					//MBOWrapper mboWrapper = new MBOWrapper(trackModelWrapper,ctcWrapper,clock);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 
- private static LinkedList<Block> track;
- 
- public static void main(String[] args) {
-  EventQueue.invokeLater(new Runnable() {
-   public void run() {
-    try {
-     TrackControllerWrapper trackControllerWrapper = new TrackControllerWrapper(track);
-    } catch (Exception e) {
-     e.printStackTrace();
-    }
-   }
-  });
- }
+	}
+
 }
