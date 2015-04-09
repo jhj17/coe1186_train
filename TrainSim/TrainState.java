2d29af0e40fc008b82c8dfd0d49abf586844b94f
Zachary Barnes <zach.a.barnes@gmail.com>
2015-04-08 21:46:47 -0400

    Update states

--- TrainSim/TrainState.java
+++ TrainSim/TrainState.java
@@ -1,23 +1,23 @@
 
 public class TrainState {
 final double SampleRate = .002;
-final double EBrakeRate = 100; //need real value
-final double SBrakeRate = 100; //need real value
+final double EBrakeRate = 2.73; //need real value
+final double SBrakeRate = 1.2; //need real value
 final double StationStopTime = 2; //need real values
 public DynamicTrainValues tv; //contains curSpeed, curAuthority, commandedSpeed, distance, curTemp
-public boolean isAutopilot;
-public boolean userEmergencyBrake;
-public boolean userServiceBrake;
-public boolean shouldEmergency; //should eBrake
-public boolean shouldService; //should servBrake
-public boolean shouldLight;
-public boolean shouldLeftDoor;
-public boolean shouldRightDoor;
+public boolean isAutopilot = false;
+public boolean userEmergencyBrake = false;
+public boolean userServiceBrake = false;
+public boolean shouldEmergency = true; //should eBrake
+public boolean shouldService = false; //should servBrake
+public boolean shouldLight = false;
+public boolean shouldLeftDoor = false;
+public boolean shouldRightDoor = false;
 public double desTemp;
 public double userDesSpeed;
-public double commandedPower;
-public boolean atStation;
-public boolean approachingStation;
+public double commandedPower = 0;
+public boolean atStation = false;
+public boolean approachingStation = false;
 public byte fails; //4 different kinds, 00,01,10,11
 public String curBeacon;
 public String stationName;
@@ -25,11 +25,11 @@ public double distToStation;
 public String stationSide; //LEFT, RIGHT
 public boolean stationAnnounced;
 public double timeOfStationArrival;
-public boolean isEmergency;
-public boolean isService;
-public boolean isLeftDoor;
-public boolean isRightDoor;
-public boolean isLight;
+public boolean isEmergency = false;
+public boolean isService = false;
+public boolean isLeftDoor = false;
+public boolean isRightDoor = false;
+public boolean isLight = false;
 public double curTime;
 	public TrainState()
 	{
