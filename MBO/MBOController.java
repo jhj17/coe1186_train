d1d3e565a89dde36a8d2c8124dec96f45cd2e61f
John Abraham <jpa18@pitt.edu>
2015-04-19 19:14:20 -0400

    Update and rename Scheduler.java to MBOController.java

---
+++ MBO/MBOController.java
@@ -0,0 +1,42 @@
+/**
+* MBO.java
+*
+* @author John Abraham
+*
+*/
+
+public class MBOController{
+
+	private boolean mboMode;
+
+	/*
+	* Calculates the safe speed based on a given train and next train's locations. 
+	* Get train location from TrainModel objects container using trainID.
+	*/
+	public double getSpeed(int trainID) {
+		if(!mboMode)
+			return -1;
+		else {
+
+
+			return 0;
+		}
+	}
+
+	/*
+	* Calculates the safe authority based on a given train's suggested speed.
+	*/
+	public double getAuthority(int trainID) {
+		if(!mboMode)
+			return -1;
+		else {
+
+			
+			return 0;
+		}
+	}
+
+	public void setMBOMode(boolean mboMode) {
+		this.mboMode = mboMode;
+	}
+}
